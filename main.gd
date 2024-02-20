09a55ca7074159986116c5cfbe5df740ada9a9d3
DoormatIka <trashcanartistlily@gmail.com>
2024-02-21 01:14:43 +0800

    graph slightly functional, bad perf

--- main.gd
+++ main.gd
@@ -20,45 +20,46 @@ func _on_load_tree_file_selected(path):
 			print("Data Received");
 			var test = {
 				"top": ["data1", "data2"],
-				"data1": ["data2", "data3"],
+				"data1": ["data", "alice"],
+				"data2": ["data3", "kyunfire"],
+				"data3": ["data4", "data5", "data6"],
 			}
-			_run_through_top_node(test)
+			_run_through_node(test, "top")
 		else:
 			print("Unexpected data")
 	else:
 		print("JSON Parse Error: ", json.get_error_message(), " at line ", json.get_error_line())
-	
+		
 	$tree_graph_edit.arrange_nodes()
 
-func _run_through_top_node(data: Dictionary):
-	for fen in data["top"]:
-		_run_through_node(data, fen)
-
 func _run_through_node(data: Dictionary, selected_key: String):
-	counter += 1
-	
 	if !data.has(selected_key):
 		return
-		
-	var start_node = GraphNode.new()
-	var start_control_left = Control.new()
-	var start_control_right = Control.new()
-	start_node.set_title(str(selected_key, counter))
-	start_node.name = str(selected_key, counter)
-	start_node.set_slot_enabled_left(0, true)
-	start_node.set_slot_type_left(0, 1)
-	start_node.set_slot_enabled_right(1, true)
-	start_node.set_slot_type_right(1, 1)
-	start_node.add_child(start_control_left)
-	start_node.add_child(start_control_right)
-	$tree_graph_edit.add_child(start_node)
+	
+	var node = get_node(str("tree_graph_edit", "/", selected_key))
+	var start_node;
+	if !node:
+		start_node = GraphNode.new()
+		var start_control_left = Control.new()
+		var start_control_right = Control.new()
+		start_node.set_title(selected_key)
+		start_node.name = selected_key
+		start_node.set_slot_enabled_left(0, true) 
+		start_node.set_slot_type_left(0, 1)
+		start_node.set_slot_enabled_right(1, true)
+		start_node.set_slot_type_right(1, 1)
+		start_node.add_child(start_control_left)
+		start_node.add_child(start_control_right)
+		$tree_graph_edit.add_child(start_node)
+	else:
+		start_node = node
 	
 	for fen in data[selected_key]:
 		var end_node = GraphNode.new()
 		var end_control_left = Control.new()
 		var end_control_right = Control.new()
-		end_node.set_title(str(fen, counter))
-		end_node.name = str(fen, counter)
+		end_node.set_title(fen)
+		end_node.name = fen
 		end_node.set_slot_enabled_left(0, true)
 		end_node.set_slot_type_left(0, 1)
 		end_node.set_slot_enabled_right(1, true)
@@ -67,8 +68,8 @@ func _run_through_node(data: Dictionary, selected_key: String):
 		end_node.add_child(end_control_right)
 		$tree_graph_edit.add_child(end_node)
 		
-		$tree_graph_edit.connect_node(str(fen, counter), 0, str(selected_key, counter), 0)
-		##_run_through_node(data, fen)
+		$tree_graph_edit.connect_node(fen, 0, selected_key, 0)
+		_run_through_node(data, fen)
 
 
 func _on_load_tree_button_pressed():
