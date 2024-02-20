8b8ec2bc5d99bbdd587a19e1307c1b59b7536215
DoormatIka <trashcanartistlily@gmail.com>
2024-02-20 14:59:04 +0800

    first commit

---
+++ FileDialog.gd
@@ -0,0 +1,8 @@
+extends FileDialog
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_confirmed():
+	print(current_file)
